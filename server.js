c32c8c2a4dfdd513fc514738f66dc505dd70329d
Kazi Farhana Faruque <168679096+Farhana-Faruque@users.noreply.github.com>
2026-01-13 17:06:05 +0600

    add files

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static('public'));
+
+const users = {};
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('join', (username) => {
+        socket.username = username;
+        users[socket.id] = username;
+
+        socket.emit('user list', Object.values(users));
+
+        socket.broadcast.emit('user joined', username);
+        console.log(`${username} joined`);
+    });
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', { user: socket.username, text: msg });
+    });
+
+    socket.on('disconnect', () => {
+        if (socket.username) {
+            delete users[socket.id];
+            io.emit('user left', socket.username); 
+            console.log(`${socket.username} left`);
+        }
+    });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
